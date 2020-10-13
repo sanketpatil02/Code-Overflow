0b2370a6f70992b498a4d07aa6c5ee61d8b93b5b
JayPatel1060 <72780261+JayPatel1060@users.noreply.github.com>
2020-10-13 12:06:22 +0530

    Program to print array in reverse order

---
+++ Program_to_reverse_array.py
@@ -0,0 +1,8 @@
+#python program to reverse the array
+
+array = [10,20,30,40,50];     
+    
+print("Array in reverse order: ");    
+#Loop through the array in reverse order    
+for i in range(len(array)-1, -1, -1):     
+    print(array[i])     
